38f2dc3b1e28f94c377fd586a18af110b9544f36
mukul-rathi <mukul.rathi@gmail.com>
2018-03-21 17:02:50 +0000

    Updated description of SortingAlgo interface

--- src/mukulrathi/algorithms/sorting/SortingAlgo.java
+++ src/mukulrathi/algorithms/sorting/SortingAlgo.java
@@ -1,6 +1,9 @@
-package mukulrathi.algorithms.sorting;/*
+package mukulrathi.algorithms.sorting;
+/*
 This is an interface that all sorting algorithms must implement
-
+(for simplicity this is referring to integer sorting, though most of the algorithms
+(i.e those except Radix and Counting sort)
+could be modified to work with other types)
  */
 
 
