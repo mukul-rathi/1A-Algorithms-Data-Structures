d2cf131f0a2ac6478255f0afe3f49c9aafbf5c84
mukul-rathi <mukul.rathi@gmail.com>
2018-03-21 16:59:56 +0000

    Added Comments to Binary Insertion Sort code

--- src/mukulrathi/algorithms/sorting/BinaryInsertionSort.java
+++ src/mukulrathi/algorithms/sorting/BinaryInsertionSort.java
@@ -5,11 +5,14 @@ public class BinaryInsertionSort implements SortingAlgo{
 This is a modification of traditional insertion sort - instead of doing a linear
 search when identifying the lowest element greater than key, we use a binary search
 to reduce number of comparisons.
+*
  */
     @Override
     public int[] sort(int[] A){
-
+        //Initially A[0] sorted (since 1 element so by definition)
         for(int i=1;i<A.length;i++){
+            /*Pre-condition: A[0..i-1] is a sorted array.
+             */
             int key = A[i];
             //binary search to find lowest element greater than the key
             int low = 0;
@@ -27,8 +30,10 @@ to reduce number of comparisons.
                 A[j] = A[j-1];
             }
             A[low] = key;
-
+        /*Post-condition: A[0..i] is a sorted array.
+             */
         }
+        //A[0....n-1] sorted i.e. A is sorted.
         return A;
     }
 }
