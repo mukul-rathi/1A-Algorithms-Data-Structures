83a7e2cf3dd43f595f1248eaff71a99b9c20ea05
mukul-rathi <mukul.rathi@gmail.com>
2018-03-23 22:35:51 +0000

    Added more description about Deque ADT

--- src/mukulrathi/datastructures/abstractdatatypes/Deque.java
+++ src/mukulrathi/datastructures/abstractdatatypes/Deque.java
@@ -5,6 +5,8 @@ import mukulrathi.customexceptions.UnderflowException;
 /*
 Deque = Double-ended queue
 This is a variant on the queue, where you can add or remove items from both ends.
+
+Stacks and Queues can be seen as restricted Deques, where only one put and one get are allowed.
  */
 public interface Deque<T> {
     boolean isEmpty(); //returns true if deque is empty, false otherwise
