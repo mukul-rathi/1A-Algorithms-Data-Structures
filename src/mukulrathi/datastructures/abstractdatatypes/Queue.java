0325c792f35cdef500064200668761ffcc298552
mukul-rathi <mukul.rathi@gmail.com>
2018-03-23 22:11:04 +0000

    Added Queue ADT code

---
+++ src/mukulrathi/datastructures/abstractdatatypes/Queue.java
@@ -0,0 +1,18 @@
+package mukulrathi.datastructures.abstractdatatypes;
+
+import mukulrathi.customexceptions.UnderflowException;
+/*
+    This is a FIFO (First-In-First-Out) ADT.
+ */
+public interface Queue<T> {
+    boolean isEmpty(); //returns true if queue is empty, false otherwise
+
+    void put(T x); //add element x to end of queue
+    //Post-condition: isEmpty()==false
+
+    T get() throws UnderflowException; //Pre-condition: isEmpty()==false
+    //remove and return first element from queue
+
+    T first() throws UnderflowException; //Pre-condition: isEmpty()==false
+        //return top element from queue without removing it
+}
