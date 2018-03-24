b2ef9874c7e2faeff90299a31e290496daf89b36
mukul-rathi <mukul.rathi@gmail.com>
2018-03-24 09:07:07 +0000

    Add implementation information in Dictionary ADT
    This is because actually implementing
    the three methods given (Vector,Lists and Arrays) is straightforward.

--- src/mukulrathi/datastructures/abstractdatatypes/Dictionary.java
+++ src/mukulrathi/datastructures/abstractdatatypes/Dictionary.java
@@ -2,7 +2,21 @@ package mukulrathi.datastructures.abstractdatatypes;
 /*
 Stores a mapping from keys to values - this is a function, i.e one key has at most one value
 associated with it.
+A dictionary is like a dynamic set optimised for lookup.
 This is a simple interface, but practical implementations may also have a containsKey(), size() etc.
+
+To implement Sets/Dictionaries we have multiple potential options:
+    Vector: Use direct addressing - if keys in range [a...b] then subtract a to give index into vector
+            O(1) lookup but inefficient space-wise if number of keys << b-a.
+
+    Lists: each node stores key,value pair - get() does linear search through list.
+            set() - either adds to front of list -> so get() would consider first item with key
+                    when traversing as actual value (lazy - means cost of get() increases)
+                    or scans through list first and replaces if key already present
+
+    Arrays: good for static sets (i.e no adding/removing) and also if keys ordered as can binary search
+            to lookup key - O(lg n).
+
  */
 public interface Dictionary<K,V> {
     void set(K k, V v); //store the (k,v) pair in dictionary -
